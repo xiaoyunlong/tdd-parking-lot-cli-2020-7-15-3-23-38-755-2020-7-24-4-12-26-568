92e618c97d79f311e44eb7e572bfb49151d5f473
xiaoyunlong <1758217010@qq.com>
2020-07-26 16:09:34 +0800

    [Parking Lot Story 1][Lebron] feature:pass  should_return_null_when_parking_car_given_car_and_no_position_parking_lot

--- src/main/java/com/oocl/cultivation/test/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/ParkingBoy.java
@@ -1,22 +1,26 @@
 package com.oocl.cultivation.test;
 
 
-
 public class ParkingBoy {
+    private int parkingLotCapacity = 3;
 
     public ParkingTicket parking(Car car) {
-
+        if (this.parkingLotCapacity <= 0) {
+            return null;
+        }
+        this.parkingLotCapacity--;
         return new ParkingTicket(car.getCarNumber());
     }
 
     public Car fetchCar(ParkingTicket parkingTicket) {
-        if(parkingTicket==null){
+
+        if (parkingTicket == null) {
             return null;
         }
-        if(parkingTicket.getTicketNumber().equals("Wrong")){
+        if (parkingTicket.getTicketNumber().equals("Wrong")) {
             return null;
         }
-        if(parkingTicket.isUsed()){
+        if (parkingTicket.isUsed()) {
             return null;
         }
         parkingTicket.setUsed(true);
